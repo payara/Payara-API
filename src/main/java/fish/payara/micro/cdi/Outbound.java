0db3b526cb91d6ff38502e06e0ad4124be10ab41
Mert ÇALIŞKAN <mert@t2.com.tr>
2017-02-21 15:13:34 +0300

    PAYARA-1453-Payara-API-jar-package-names-dont-match-what-is-in-the-server

--- src/main/java/fish/payara/micro/cdi/Outbound.java
+++ src/main/java/fish/payara/micro/cdi/Outbound.java
@@ -1,4 +1,5 @@
 /*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2016 Payara Foundation and/or its affiliates. All rights reserved.
  *
@@ -36,20 +37,49 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package fish.payara.requesttracing.api;
+package fish.payara.micro.cdi;
 
-import javax.interceptor.InterceptorBinding;
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.*;
 
 /**
- * @author mertcaliskan
+ * Annotation to be applied to a CDI event @Inject point to send it remotely via CDI event bus. Such events can be observed using the {@link Inbound} qualifier.
+ *
+ * @author steve
  */
-@InterceptorBinding
-@Target({ TYPE, METHOD })
-@Retention(RUNTIME)
-public @interface Traced {}
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+@Target({METHOD, FIELD, PARAMETER})
+public @interface Outbound {
+
+    /**
+     * Provides a further level of filtering. Specify an eventname to restrict
+     * event callbacks to events with the specific name
+     * @return
+     */
+    @Nonbinding
+    String eventName() default "";
+
+    /**
+     * Property to set whether the message should also fire on the same instance as well
+     * default is false it won't be fired as an Inbound message on the same instance
+     * @return
+     */
+    @Nonbinding
+    boolean loopBack() default false;
+
+    /**
+     * Property to restrict the outbound event to specific named server or micro instances.
+     * Default behavior is to fire on all server and micro instances.
+     * Set one or more instance names to restrict the event to firing only
+     * on the specified instances.
+     * @return
+     */
+    @Nonbinding
+    String[] instanceName() default "";
+}
